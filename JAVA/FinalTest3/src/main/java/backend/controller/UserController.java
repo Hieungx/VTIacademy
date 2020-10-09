ac16eff548dd4b4b45a273668df37a66be9a5061
hieungx <hieuu99@gmail.com>
2020-10-09 23:16:12 +0700

    Final Test ver 1

---
+++ JAVA/FinalTest3/src/main/java/backend/controller/UserController.java
@@ -0,0 +1,45 @@
+package backend.controller;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import backend.service.IUserService;
+import backend.service.UserService;
+import entity.Project;
+
+public class UserController {
+	private IUserService userService;
+
+	public UserController() throws FileNotFoundException, IOException {
+		userService = new UserService();
+	}
+
+	public void registerManager(String firstName, String lastName, String Phone, String email, String pass,
+			int expInYear) throws SQLException {
+		userService.registerManager(firstName, lastName, Phone, email, pass, expInYear);
+	}
+
+	public void registerEmployee(String firstName, String lastName, String Phone, String email, String pass,
+			String projectName, String proSkill) throws SQLException {
+		userService.registerEmployee(firstName, lastName, Phone, email, pass, projectName, proSkill);
+	}
+
+	public boolean isUserExists(String email, String pass) throws SQLException {
+		return userService.isUserExists(email, pass);
+	}
+	
+	public boolean isEmployeeExists(String email, String pass) throws SQLException {
+		return userService.isEmployeeExists(email, pass);
+	}
+	
+	public boolean isProjectExists(int id) throws SQLException {
+		return userService.isProjectExists(id);
+	}
+	
+//	public List<Project> ShowUserInProject() throws SQLException {
+//		return IUserService.showUserInProject();
+//	}
+
+}
---
+++ JAVA/FinalTest3/src/main/java/utils/ScannerUtils.java
@@ -0,0 +1,121 @@
+package utils;
+
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ScannerUtils {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static int inputInt(String errorMessage) {
+		while (true) {
+			try {
+				int number = Integer.parseInt(scanner.nextLine().trim()); // trim để xóa khoảng trắng
+
+				if (number >= 0) {
+					return number;
+				} else
+					System.err.println(errorMessage);
+
+			} catch (Exception e) {
+				System.err.println(errorMessage);
+			}
+		}
+	}
+
+	public static float inputFloat(String errorMessage) {
+		while (true) {
+			try {
+				float number = Float.parseFloat(scanner.nextLine().trim());
+				return number;
+
+			} catch (Exception e) {
+				System.err.println(errorMessage);
+			}
+		}
+	}
+
+	public static double inputDouble(String errorMessage) {
+		while (true) {
+			try {
+				return Double.parseDouble(scanner.nextLine().trim());
+			} catch (Exception e) {
+				System.err.println(errorMessage);
+			}
+		}
+	}
+
+	public static String inputString(String errorMessage) {
+		while (true) {
+			String str = scanner.nextLine().trim();
+			if (!str.isEmpty()) {
+				return str;
+			} else {
+				System.err.println(errorMessage);
+			}
+		}
+	}
+
+	public static LocalDate inputLocalDate() {
+		while (true) {
+			String date = "";
+			LocalDate localDate = null;
+			DateTimeFormatter formatter = null;
+			try {
+				date = ScannerUtils.inputString("ChÆ°a nháº­p ngÃ y. Vui lÃ²ng nháº­p láº¡i!");
+				formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+				localDate = LocalDate.parse(date, formatter);
+				return localDate;
+			} catch (DateTimeParseException e) {
+				System.err.println("Vui lÃ²ng nháº­p ngÃ y theo format dd-MM-yyyy!");
+			}
+		}
+	}
+
+	public static LocalDateTime inputLocalDateTime() {
+		while (true) {
+			String date = "";
+			LocalDateTime localDate = null;
+			DateTimeFormatter formatter = null;
+			try {
+				date = ScannerUtils.inputString("ChÆ°a nháº­p ngÃ y. Vui lÃ²ng nháº­p láº¡i!");
+				formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+				localDate = LocalDateTime.parse(date, formatter);
+				return localDate;
+			} catch (DateTimeParseException e) {
+				System.err.println("Vui lÃ²ng nháº­p ngÃ y theo Ä‘á»‹nh dáº¡ng dd-MM-yyyy HH:mm:ss!");
+			}
+		}
+	}
+
+	public static String convertLocalDateTimeToString(LocalDateTime date) {
+
+		String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		return formattedDate;
+	}
+
+	public static LocalDate convertStringToLocalDate(String dateStr) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate localDate = LocalDate.parse(dateStr, formatter);
+		return localDate;
+	}
+
+	public static String convertLocalDateToString(LocalDate date) {
+
+		String formattedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		return formattedDate;
+	}
+
+	public static String convertDoubleToMoneyVND(double total) {
+		Locale localeVN = new Locale("vi", "VN");
+		NumberFormat currencyVN = NumberFormat.getCurrencyInstance(localeVN);
+		String str = currencyVN.format(total);
+
+		return str;
+	}
+}
